d7bc7affbf9c590bef7b68a5dd82bca6a0d721d4
gerica <rogeriogerica@gmail.com>
2021-08-29 12:05:01 -0300

    info tela home

--- lib/ui/page/homePage.dart
+++ lib/ui/page/homePage.dart
@@ -13,6 +13,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    TextStyle? bodyText2 = Theme.of(context).textTheme.bodyText2;
     return Scaffold(
       appBar: AppBar(
         title: Text('Fundamentalistas'),
@@ -21,10 +22,51 @@ class _HomePageState extends State<HomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [],
+          children: [
+            RichText(
+              text: TextSpan(
+                style: bodyText2,
+                children: <TextSpan>[
+                  TextSpan(text: 'Fórmula mágica de '),
+                  TextSpan(text: 'Joel Greenblatt', style: TextStyle(fontWeight: FontWeight.bold)),
+                ],
+              ),
+            ),
+            SizedBox(height: 5),
+            RichText(
+              text: TextSpan(
+                style: bodyText2,
+                children: <TextSpan>[
+                  TextSpan(text: 'As empresas de maior '),
+                  TextSpan(text: 'ROE ', style: TextStyle(fontWeight: FontWeight.bold)),
+                  TextSpan(text: 'são as mais rentáveis'),
+                ],
+              ),
+            ),
+            SizedBox(height: 5),
+            RichText(
+              text: TextSpan(
+                style: bodyText2,
+                children: <TextSpan>[
+                  TextSpan(text: 'As empresas de menor '),
+                  TextSpan(text: 'P/L ', style: TextStyle(fontWeight: FontWeight.bold)),
+                  TextSpan(text: 'são as mais baratas'),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+// Fórmula mágica de Joel Greenblatt
+// Para ficar mais fácil de de você entender, vamos fazer um passo a passo simplificado de como Joel Greenblatt usa a sua fórmula mágica para encontrar as melhores barganhas da Bolsa de Valores..
+//
+// Escolha alguns papéis que lhe interessam para compor seu portfólio;
+// Calcule o ROE e o índice P/L de cada um dos ativos;
+// Construa 2 Rankings: As empresas de maior ROE são as mais rentáveis e as de menor P/L são as mais baratas;
+// Some a posição de cada companhia em cada um dos rankings e ordena estas somas em ordem crescente;
+// Resultado: As empresas de menor soma no ranking combinado são aquelas que teriam a melhor combinação de índices.
